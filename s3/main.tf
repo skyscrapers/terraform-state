6c856a232e71b3ba2f3a166d4be07dccd5a21d74
iuri aranda <iuri.aranda@gmail.com>
2019-01-18 15:38:53 +0100

    Secure and simplify the s3 module (#9)
    - Enable server-side encryption by default
    - Restrict any public access to the bucket
    - Remove variables to make the creation of s3 bucket and dynamodb table optional (not needed anymore)
    - Remove variable to share the s3 bucket with other accounts (not needed anymore)

--- s3/main.tf
+++ s3/main.tf
@@ -1,10 +1,17 @@
 resource "aws_s3_bucket" "state" {
-  count  = "${var.create_s3_bucket == "true" ? 1 : 0}"
   bucket = "terraform-remote-state-${var.project}"
   acl    = "private"
 
   versioning {
     enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
   }
 
   tags {
@@ -13,8 +20,15 @@ resource "aws_s3_bucket" "state" {
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket                  = "${aws_s3_bucket.state.id}"
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "b" {
-  count  = "${var.create_s3_bucket == "true" ? 1 : 0}"
   bucket = "${aws_s3_bucket.state.bucket}"
 
   policy = <<EOF
@@ -45,43 +59,13 @@ resource "aws_s3_bucket_policy" "b" {
           "s3:x-amz-server-side-encryption": "true"
         }
       }
-    }${length(data.template_file.cross_account_bucket_sharing_policy.*.rendered) == 0 ? format("%s","") : format("%s", join("", data.template_file.cross_account_bucket_sharing_policy.*.rendered))}
+    }
   ]
 }
 EOF
 }
 
-data "template_file" "cross_account_bucket_sharing_policy" {
-  count = "${length(var.shared_aws_account_ids) > 0 && var.create_s3_bucket == "true" ? 1 : 0}"
-
-  template = <<EOF
-,{
-   "Sid": "Shared bucket permissions",
-   "Effect": "Allow",
-   "Principal": {
-      "AWS": [ $${principal_aws} ]
-   },
-   "Action": [
-      "s3:GetBucketLocation",
-      "s3:ListBucket",
-      "s3:GetObject*",
-      "s3:PutObject*"
-   ],
-   "Resource": [
-      "$${bucket_arn}",
-      "$${bucket_arn}/*"
-   ]
-}
-EOF
-
-  vars {
-    principal_aws = "${join(",", formatlist("\"arn:aws:iam::%s:root\"", var.shared_aws_account_ids))}"
-    bucket_arn    = "${aws_s3_bucket.state.arn}"
-  }
-}
-
 resource "aws_dynamodb_table" "terraform_state_locktable" {
-  count          = "${var.create_dynamodb_lock_table == "true" ? 1 : 0}"
   name           = "terraform-remote-state-lock-${var.project}"
   read_capacity  = 1
   write_capacity = 1
--- s3/outputs.tf
+++ s3/outputs.tf
@@ -1,11 +1,11 @@
 output "bucket_id" {
+  value       = "${aws_s3_bucket.state.id}"
   description = "Id (name) of the S3 bucket"
-  value       = "${join("", aws_s3_bucket.state.*.id)}"
 }
 
 output "locktable_id" {
+  value       = "${aws_dynamodb_table.terraform_state_locktable.id}"
   description = "Id (name) of the DynamoDB lock table"
-  value       = "${join("", aws_dynamodb_table.terraform_state_locktable.*.id)}"
 }
 
 output "tf_policy_name" {
