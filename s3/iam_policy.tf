bf29d04e4e1e99b4bb74be66320bc4da0adc870e
iuri aranda <iuri.aranda@gmail.com>
2019-03-29 15:44:09 +0100

    Allow s3:DeleteObject on tf state bucket (#10)
    Otherwise we cannot delete a terraform workspace.
    As it is a versioned bucket and the iam policy doesn't give s3:DeleteObjectVersion permissions, all deleted state files will remain in the bucket as old versions.

--- s3/iam_policy.tf
+++ s3/iam_policy.tf
@@ -11,6 +11,7 @@ data "aws_iam_policy_document" "tf" {
     actions = [
       "s3:GetObject",
       "s3:PutObject",
+      "s3:DeleteObject",
     ]
 
     resources = ["${aws_s3_bucket.state.arn}/*"]
